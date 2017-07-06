4dd7534280e63fcb735186e35a5e7b8d3b94247a
Matt Angus <mattangus02@gmail.com>
2017-07-05 20:14:03 -0400

    added a plotter and some plots

--- ARC/index.md
+++ ARC/index.md
@@ -10,7 +10,7 @@ modified: 2017-04-07
 # About the club
 ---
 
-The Autonomous Robotics Club (ARC) was founded with the goal of competing in the Intelligent Ground Vehicle Competition (IGVC). The robot - TAURUS - was entered in
+The Autonomous Robotics Club (ARC) was founded with the goal of competing in the Intelligent Ground Vehicle Competition (IGVC). The robot - Taurus - was first entered into IGVC in 2016, where we won the Rooky of the year award. Taurus uses sensors such as GPS, LiDAR and Compass to determine its position in the environment. A list of components is below. Taurus runs on a small laptop that is mounted on the rear, and coded in C++.
 
 # Hardware
 ---
@@ -36,4 +36,5 @@ The Autonomous Robotics Club (ARC) was founded with the goal of competing in the
 The whole system was implemented in C++. Consisting of around 2700 lines of code. The system was running at about 0.6 seconds per iteration of the main loop. Each iteration consisted of collecting data from each sensor, processing the data and outputting a command to the actuators.
 
 ## Fuzzy Logic
-Fuzzy Logic was used as the motion planner. 
+Fuzzy logic is a system of non binary logic. Instead of truth values (true/1 and false/0), statements are assigned a value between 0 and 1 inclusive. This value is called the membership value. It represents the membership of a set or concept. We will call these Fuzzy Sets. Fuzzy Logic was used as the motion planner. The controller consisted of three fuzzy sets: Near-Far, Target, and Free-Space.
+
---
+++ plotter.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def bell_fn(x, a, b, c):
+	inner = (x - c) / a;
+	outer = 1 + (pow(abs(inner), 2.0 * b));
+	return 1 / outer;
+
+def bell(x, a, b, c, greater, less):
+	y = np.zeros(x.shape)
+	for i in range(len(x)):
+		if greater and x[i] > c:
+			y[i] = 1
+		elif less and x[i] < c:
+			y[i] = 1
+		else:
+			y[i] = bell_fn(x[i], a, b, c)
+	return y
+
+def inv_bell(x, a, b, c, right):
+	y = np.zeros(x.shape)
+	for i in range(len(x)):
+		if right and x[i] < c:
+			y[i] = 0
+		elif not right and x[i] > c:
+			y[i] = 0
+		else:
+			y[i] = 1 - bell_fn(x[i], a, b, c)
+	return y
+
+def gauss_fn(x, c, s):
+	inner = (x - c) / s;
+	temp = np.exp((-0.5) * (inner * inner));
+	return temp;
+
+def gauss(x, c, s, greater, less):
+	y = np.zeros(x.shape)
+	for i in range(len(x)):
+		if greater and x[i] > c:
+			y[i] = 1
+		elif less and x[i] < c:
+			y[i] = 1
+		else:
+			y[i] = gauss_fn(x[i], c, s)
+	return y
+
+def inv_gauss(x, c, s, right):
+	y = np.zeros(x.shape)
+	for i in range(len(x)):
+		if right and x[i] < c:
+			y[i] = 0
+		elif not right and x[i] > c:
+			y[i] = 0
+		else:
+			y[i] = 1 - gauss_fn(x[i], c, s)
+	return y
+
+
+def near_far():
+	x = np.arange(-1.0, 4.0, 0.005)
+	near = bell(x, 1, 3, 0.5, False, True)
+	far = bell(x, 1, 3, 2.5, True, False)
+	near_line, = plt.plot(x, near)
+	far_line, = plt.plot(x, far)
+	plt.legend([near_line, far_line], ['Near', 'Far'])
+	plt.grid(True)
+	plt.show()
+
+def target():
+	x = np.arange(-40, 40, 0.01)
+	right = inv_bell(x, 15.0, 3.0, 0.0, False)
+	ahead = bell(x, 15.0, 3.0, 0.0, False, False)
+	left = inv_bell(x, 15.0, 3.0, 0.0, True)
+	right_line, = plt.plot(x, right)
+	ahead_line, = plt.plot(x, ahead)
+	left_line, = plt.plot(x, left)
+	plt.legend([right_line, ahead_line, left_line], ['Right', 'Ahead', 'Left'])
+	plt.grid(True)
+	plt.show()
+
+def free():
+	x = np.arange(-20, 20, 0.01)
+	right = inv_gauss(x, 0.0, 5.0, False)
+	ahead = gauss(x, 0.0, 5.0, False, False)
+	left = inv_gauss(x, 0.0, 5.0, True)
+	right_line, = plt.plot(x, right)
+	ahead_line, = plt.plot(x, ahead)
+	left_line, = plt.plot(x, left)
+	plt.legend([right_line, ahead_line, left_line], ['Right', 'Ahead', 'Left'])
+	plt.grid(True)
+	plt.show()
+
+free()
